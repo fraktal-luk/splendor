b095fd83f9666f5aea7ca129ff5b3263ef912a5b
fraktal-luk <fraktal3725@gmail.com>
2024-12-29 18:12:27 +0100

    Sorting states

--- simple_tree.ts
+++ simple_tree.ts
@@ -15,57 +15,9 @@ const table = tree.root.state.table;
 
 console.log(table.rows);
 
-
+let setVar = new Set<string>();
 
 function iters1(times: number): void {
-	let nBuys = 0;
-	let nTakes = 0;
-
-	let front = [tree.root.state];
-
-	let iter = 0;
-	
-	while (iter++ <= times) {
-		let totalLevelSize = 0;
-
-		let strSet = new Set<string>();
-
-		for (let i = 0; i < front.length; i++) {
-			let s = front[i]!;
-
-			const newTakes = s.findTakes();
-			const newBuys = s.findBuys();
-
-			nBuys += newBuys.length;
-			nTakes += newTakes.length;
-
-			totalLevelSize += (newTakes.length + newBuys.length);
-			
-			for (const e of newTakes)
-				strSet.add(e.str());
-			
-			for (const e of newBuys)
-				strSet.add(e.str());
-
-		}
-
-		const uniqueStrs = Array.from(strSet.values());
-		
-		const recreated = uniqueStrs.map(s => GameState1.fromStr(s));
-
-		console.log('All ' + totalLevelSize + ", unique " + strSet.size);
-		
-		front = recreated;
-	}
-
-	console.log('\n');
-	console.log(front.length);
-	console.log(nTakes);
-	console.log(nBuys);
-}
-
-
-function iters2(times: number): void {
 	let nBuys = 0;
 	let nTakes = 0;
 
@@ -80,41 +32,74 @@ function iters2(times: number): void {
 
 		for (const ss of front) {
 			let s = GameState1.fromStr(ss);
-
 			const newTakes = s.findTakes();
 			const newBuys = s.findBuys();
 
 			nBuys += newBuys.length;
 			nTakes += newTakes.length;
-
 			totalLevelSize += (newTakes.length + newBuys.length);
 			
-			for (const e of newTakes)
-				strSet.add(e.str());
-			
-			for (const e of newBuys)
-				strSet.add(e.str());
-
+			for (const e of newTakes) strSet.add(e.str());
+			for (const e of newBuys) strSet.add(e.str());
 		}		
 
 		console.log('All ' + totalLevelSize + ", unique " + strSet.size);
 		front = strSet;
 	}
 
-	console.log('\n');
-	console.log(front.size);
-	console.log(nTakes);
-	console.log(nBuys);
+	setVar = front;
+
+	console.log('\n' + front.size + ";; " + nTakes + "; " + nBuys);
 }
 
 
-const times = 6;
+const times = 2;
 
 
 console.log('\n\n');
 
 console.time('1');
-iters2(times);
+iters1(times);
 console.timeEnd('1');
 
 console.log(process.memoryUsage());
+
+
+function cmpStates1(a: GameState1, b: GameState1): number {
+	// First by points
+	const pointDiff = b.player.points - a.player.points;
+	if (pointDiff != 0) return pointDiff;
+	
+	// Compare bonuses
+	const bonusDiff = vecSum(vecSub(b.player.bonuses, a.player.bonuses));
+	if (bonusDiff != 0) return bonusDiff;
+
+	// Compare bonuses
+	const tokenDiff = vecSum(vecSub(b.player.tokens, a.player.tokens));
+	if (tokenDiff != 0) return tokenDiff;
+
+	return 0;
+}
+
+
+
+const fs = require("fs");
+
+if (setVar.size > 2000) {//throw new Error("Too big to write to file");
+	console.log("Too big to write to file");
+	process.exit(0);
+}
+let writer = fs.createWriteStream("states.txt");
+
+const states = [...setVar].map(s => GameState1.fromStr(s));
+const sorted = states.sort(cmpStates1);
+
+for (const x of sorted) {
+	const state = x;//GameState1.fromStr(x);
+					
+	const bonusSum = state.player.bonuses.reduce((a,b) => a+b);
+	const tokenSum = state.player.tokens.reduce((a,b) => a+b);
+	const str = "" + state.player.points + "; " + state.player.bonuses + " (" + bonusSum + "); " + state.player.tokens + " (" + tokenSum + ")\n";
+	writer.write(str);
+}
+writer.end();
