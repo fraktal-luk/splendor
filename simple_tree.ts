8d2d67b942cd9c3acae9f206ec03c7fa873744aa
fraktal-luk <fraktal3725@gmail.com>
2024-12-30 19:58:51 +0100

    Finding player states with nooptimal token possession

--- simple_tree.ts
+++ simple_tree.ts
@@ -1,6 +1,6 @@
 
 import {ValVector, satisfies, vecAdd, vecSub, vecEnough, vecSum,
-		vecNonNegative, vecLimit0, str2vv, vv2str	} from './valvec.ts'
+		vecNonNegative, vecLimit0, str2vv, vv2str, vvLessThan} from './valvec.ts'
 import {Color, CardId, Game, setupStacks, getCardPrice, getCardPoints, getCardColor,
 		} from './rules.ts';
 
@@ -53,7 +53,7 @@ function iters1(times: number): void {
 }
 
 
-const times = 2;
+const times = 5;
 
 
 console.log('\n\n');
@@ -92,7 +92,7 @@ if (setVar.size > 10000) {//throw new Error("Too big to write to file");
 //let writer = fs.createWriteStream("states.txt");
 
 const states = [...setVar].map(s => GameState1.fromStr(s));
-const sorted = states.sort(cmpStates1);
+const sorted = states.sort(cmpStates1); // !! this sorting probably useless in this case
 
 //let stateMap = new Map();
 
@@ -138,24 +138,35 @@ function writeStructured(states: string[][][][], fname: string): void {
 		
 	const writer = fs.createWriteStream(fname);
 
-	for (const a0 of states) {
+	for (let i0 = 0; i0 < states.length; i0++) {
+		if (states[i0] == undefined) continue;
 		
-		if (a0 == undefined) continue;
-		for (const a1 of a0) {
+		for (let i1 = 0; i1 < states[i0].length; i1++) {
+			if (states[i0][i1] == undefined) continue;
 			
-			if (a1 == undefined) continue;
-			for (const a2 of a1) {
-				if (a2 == undefined) continue;
+			for (let i2 = 0; i2 < states[i0][i1].length; i2++) {
+				if (states[i0][i1][i2] == undefined) continue;
 				
-				const state = GameState1.fromStr(a2[0]);
-				const bonusSum = state.player.bonuses.reduce((a,b) => a+b);
-				const tokenSum = state.player.tokens.reduce((a,b) => a+b);
+
+	// for (const a0 of states) {
+		
+		// if (a0 == undefined) continue;
+		// for (const a1 of a0) {
+			
+			// if (a1 == undefined) continue;
+			// for (const a2 of a1) {
+				// if (a2 == undefined) continue;
 				
-				const index = [state.player.points, bonusSum, tokenSum];
+				// const player = PlayerState1.fromStr(a2[0]);
+				// const bonusSum = player.bonuses.reduce((a,b) => a+b);
+				// const tokenSum = player.tokens.reduce((a,b) => a+b);
 				
-				const str = ">>>  " + state.player.points + "; " + bonusSum + "; " + tokenSum + "\n";
+				//const index = [];//[player.points, bonusSum, tokenSum];
+							  
+				
+				const str = ">>>  " + i0 + "; " + i1 + "; " + i2 + "\n";
 				writer.write(str);
-				writer.write(a2.join('\n'));
+				writer.write(states[i0][i1][i2].join('\n'));
 				writer.write('\n');
 			}
 		}
@@ -166,10 +177,78 @@ function writeStructured(states: string[][][][], fname: string): void {
 
 writeStructured(stateStruct, "states_structured.txt");
 
+
+function findTokenDom(str: string, others: string[][][][], bound: number[]): string | null {
+
+	for (let i0 = others.length-1; i0 >= bound[0]!; i0--) {
+		if (others[i0] == undefined) continue;
+		
+		for (let i1 = others[i0].length-1; i1 >= bound[1]!; i1--) {
+			if (others[i0][i1] == undefined) continue;
+			
+			for (let i2 = others[i0][i1].length-1; i2 > bound[2]!; i2--) { // Here strictly greater is the condition! 
+				if (others[i0][i1][i2] == undefined) continue;
+				
+				// Now compare to each element
+				let thisList = others[i0][i1][i2];
+				
+				for (const s of thisList) {
+					const checkedVec = str2vv(str.substr(0, 6));
+					const refVec = str2vv(s.substr(0, 6));
+					
+					if (vvLessThan(checkedVec, refVec))
+						return s;
+				}
+				
+			}
+		}
+	}
+
+
+	return null;
+}
+
+
+// Remove entries where num of tokens could be better (states exists which strictly dominate this in token number and aren't worse in other indicators)
 function pruneStates(states: string[][][][]): string[][][][] {
-	let res: string[][][][] = [];
+	let nOk = 0;
+	let nNok = 0;
+
+	let res: string[][][][] = //[];
+								states;
 	
+	for (let i0 = 0; i0 < states.length; i0++) {
+		if (states[i0] == undefined) continue;
+		
+		for (let i1 = 0; i1 < states[i0].length; i1++) {
+			if (states[i0][i1] == undefined) continue;
+			
+			for (let i2 = 0; i2 < states[i0][i1].length; i2++) {
+				if (states[i0][i1][i2] == undefined) continue;
+				
+				// Scan all content and compare with more promising lists
+				// Look in bins with [>=i0][>=i1][>i2]
+				let thisList = states[i0][i1][i2];
+				
+				for (let si = 0; si < thisList.length; si++) {
+					const found = findTokenDom(thisList[si], states, [i0, i1, i2]);
+					
+					if (found == null) {
+						thisList[si] += " - OK";
+						nOk++;
+					}
+					else {
+						thisList[si] += " - < " + found
+						nNok++;
+					}
+				}
+				
+			}
+		}
+		
+	}
 	
+		console.log(`OK ${nOk}, NOK ${nNok}`);
 	
 	return res;
 }
