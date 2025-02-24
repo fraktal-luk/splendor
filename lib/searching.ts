aa01815221f4d9b15bc4682a7bea842f3f1816c2
fraktal-luk <fraktal3725@gmail.com>
2025-02-24 21:32:30 +0100

    Created type for storing card state

--- lib/searching.ts
+++ lib/searching.ts
@@ -11,6 +11,30 @@ getReturns
 } from './comb.ts';
 
 
+// each stack is drained form the end, 4 last elements in each are on the table from the start
+export const TABLE_STACKS: number[][] =
+[
+  [ // lowest cards
+    62, 65, 51, 82, 74, 63, 64, 56, 69, 81,
+    55, 54, 73, 71, 57, 79, 76, 70, 85, 89,
+    78, 68, 72, 58, 59, 66, 77, 86, 87, 60,
+    61, 52, 84, 90, 83, 80, 75, 88, 67, 53
+  ],
+  [
+    27, 25, 29, 38, 36, 35, 22, 45, 41,
+    37, 40, 34, 47, 49, 26, 50, 46, 33,
+    43, 32, 21, 42, 28, 23, 30, 48, 31,
+    39, 44, 24
+  ],
+  [
+     3, 10,  5, 11, 17, 7, 19,
+    15, 16,  1, 18,  9, 6, 14,
+    12,  4, 20,  2, 13, 8
+  ]
+];
+
+
+
 function take2ifPossible(take: string, table: string): string | null {
 	const lenA = take.length;
 	for (let i = 0; i < 6; i++) {
@@ -156,5 +180,53 @@ export function states2map(states: TokenState[]): Map<string, TokenState> {
 export function moveFront(states: TokenState[]): TokenState[] {
 	const next = states.map(s => s.nextStatesUnique()).flat();
 	return statesUnique(next);
+}
+
+
+
+class PlayerCardState {
+	// 90 cards, numbers 1 to 90
+	// 8*12 = 96 -> 12 bytes contains all possible subsets of cards
+	// bit [0] is unused, [1:90] represent cards
+	str: string = "";
+	
+	has(c: number): boolean {
+		return false;
+	}
+	
+	acquire(c: number): void {
+		
+	}
+	
+	toArr(): number[] {
+		return [];
+	}
+}
+
+
+
+const INITIAL_TABLE_NUMS: number[] =
+		[ 53, 67, 75, 88,
+		  24, 31, 39, 44, 
+		   2,  8, 13, 20 ];
+const INITIAL_TABLE_STR: string = INITIAL_TABLE_NUMS.map(n => (n+256).toString(16).substr(1,2)).join('');
+
+
+class TableCardState {
+	// the order of cards on stacks is determined by a global variable (so far const presetOrder)
+	
+	levels: number[] = [36, 26, 16];
+	// 12 bytes: 1 per card
+	// 1 [][][][] bytes 0:3
+	// 2 [][][][] bytes 4:7
+	// 3 [][][][] - highest card values, bytes 8:11
+	rows: string = // rows on the table are always sorted (per row!) to prevent exploding number of equivalent states
+		INITIAL_TABLE_STR;
+}
+
+
+export class CardState {
+	player: PlayerCardState = new PlayerCardState();
+	table: TableCardState = new TableCardState();
 }
 
