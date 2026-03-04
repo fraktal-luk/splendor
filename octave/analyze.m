6b8489aba55ee4eed6a0bc2e0333a9d323945f41
fraktal-luk <fraktal3725@gmail.com>
2026-03-04 17:22:15 +0100

    Some further improvements

--- octave/analyze.m
+++ octave/analyze.m
@@ -2,7 +2,7 @@
 prefix = '../saved_3/';
 
 fh = fopen([prefix, 'followers']);
-data = fread(fh, 'float32');
+followerData = fread(fh, 'float32');
 
 fhv = fopen([prefix, 'values']);
 valueVector = fread(fhv, 'float32');
@@ -10,64 +10,53 @@ valueVector = fread(fhv, 'float32');
 fhs = fopen([prefix, 'strings']);
 stringVector = fread(fhs, 'uint16');
 
-
-dataMat = single(reshape(data, 13, []));
-stringMat = uint16(reshape(stringVector, 20, []));
-
-clear data stringVector
-
-dataMat(:,1) = []; % remove column 0 to make column 1 appear at index 1 (no big loss)
-valueVector(1) = [];
-stringMat(:,1) = [];
-% So, state 0 is absent here
-
-
-dataMat(dataMat == -1) = nan;
-
 fclose(fh);
 fclose(fhv);
 fclose(fhs);
 
-%LIMIT = 6000000; %width(dataMat); % 50000;
+followerMat = single(reshape(followerData, 13, []));
+stringMat = uint16(reshape(stringVector, 20, []));
 
-%if LIMIT > 2000000; error('WTF!'); end
+clear followerData stringVector
 
-  % !!!! Cutting to 10k for dev because performance
-    dataMat = dataMat;
- % dataMat = dataMat(:, 1:LIMIT);
-    valueVector = valueVector;
- % valueVector = valueVector(1:LIMIT);
-    stringMat = stringMat;
- % stringMat = stringMat(:, 1:LIMIT);
+followerMat(:,1) = []; % remove column 0 to make column 1 appear at index 1 (no big loss)
+valueVector(1) = [];
+stringMat(:,1) = [];
+% So, state 0 is absent here
 
-nStates = width(dataMat);
+followerMat(followerMat == -1) = nan;
 
-stepValues = countSteps(dataMat);
 
-win0 = valueVector > 0;
-win1 = valueVector < 0;
-draw = valueVector == 0;
+nStates = width(followerMat);
 
 
 [moves, points0, points1] = parsePoints(stringMat);
 
+%% Basic info is ready now 
+
     clear stringMat
+
+stepValues = countSteps(followerMat);
+
 
 diffVector = points0 - points1;
 
 
 % All edges (except 0-1)
 [statesRep, ~] = meshgrid(1:nStates, 1:13);
-edgesFrom = statesRep(~isnan(dataMat));
-edgesTo = dataMat(~isnan(dataMat));
+edgesFrom = statesRep(~isnan(followerMat));
+edgesTo = followerMat(~isnan(followerMat));
+
+    clear statesRep
 
 valuesFrom = valueVector(edgesFrom);
 valuesTo = valueVector(edgesTo); % some destinations may be outside LIMIT
 groupsFrom = stepValues(edgesFrom); % groups are defined by steps from 0
 groupsTo = stepValues(edgesTo); % should be groupsFrom + 1?, but be careful
 
-optimals = markOptimalMoves(valueVector, dataMat);
 
+
+optimals = markOptimalMoves(valueVector, followerMat);
 
 classes = char(size(valueVector));
 classes(isnan(valueVector)) = 'U';
@@ -81,4 +70,4 @@ classCounts = classifyPerGroup(classes, stepValues);
 trTable = makeTransitionHist(edgesFrom, edgesTo, classes);
 trTablesG = makeTransitionHistPerGroup(edgesFrom, edgesTo, classes, groupsFrom);
 
-[finals, tips, branching] = getCategs(points0, points1, valueVector, moves, dataMat);
+[finals, tips, branching] = getCategs(points0, points1, valueVector, moves, followerMat);
--- octave/plots.m
+++ octave/plots.m
@@ -1,31 +1,23 @@
 
-
 plotValues = makeDisplayValues(valueVector);
 
 
-  % Columns 1 through 9
-  %     335035      335036      335037      335038      335039      335040      335041      335077      335078
-  % Columns 10 through 11
-  %     335079      335080
-
-chosenState = 143632;%335035; %3789;
-followersRaw = dataMat(:, chosenState);
+chosenState = 143632;
+followersRaw = followerMat(:, chosenState);
 followers = followersRaw(~isnan(followersRaw));
 
 plotStates = [chosenState; followers];
 
-% scatter(stepValues(plotStates), plotValues(plotStates), 'r')
-
     stepsFromChosenInit = inf(1, numel(valueVector));
     stepsFromChosenInit(chosenState) = 0;
     
-    stepsFromChosen = countStepsGeneral(dataMat, stepsFromChosenInit);
+    stepsFromChosen = countStepsGeneral(followerMat, stepsFromChosenInit);
     
     % size of subtree starting from that state
     chosenNodes = stepsFromChosen < Inf;
     nnz(chosenNodes)
 
-if (nnz(chosenNodes) > 2000); error('too big graph would be'); end
+if (nnz(chosenNodes) > 3000); error('too big graph would be'); end
 
 % If subtree not too huge, we can plot it all and observe its properties
 
@@ -50,8 +42,8 @@ chTip = chTo(tips(chTo));
 scatter3(stepValues(chFrom)', chFrom, plotValues(chFrom), 'bo')
 hold on
 
-    scatter3(stepValues(chFinal)', chFinal, plotValues(chFinal), 'kd', 'filled')
-    scatter3(stepValues(chTip)', chTip, plotValues(chTip), 'rp', '')
+scatter3(stepValues(chFinal)', chFinal, plotValues(chFinal), 'kd', 'filled')
+scatter3(stepValues(chTip)', chTip, plotValues(chTip), 'rp', '')
 
 quiver3(stepValues(chFrom)', chFrom, plotValues(chFrom), vx, vy, vz,  0, 'k', 'ShowArrowHead', 'off')
 %quiver(stepValues(chFrom)', plotValues(chFrom), vx, vz, 0, 'ShowArrowHead', 'off')
