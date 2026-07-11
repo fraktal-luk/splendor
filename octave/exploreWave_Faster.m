02be8a12913f37338f836ca657596919903bc1c3
fraktal-luk <fraktal3725@gmail.com>
2026-07-11 18:46:02 +0200

    Dont backtrack without a reason

--- octave/exploreWave_Faster.m
+++ octave/exploreWave_Faster.m
@@ -1,8 +1,18 @@
-% TODO
 
-function exploreWave_Faster(graphInfo, mainTable, initialStates)
-    MAX_ITERS = 26;
+function statsTable = exploreWave_Faster(graphInfo, mainTable, initialStates)
+    MAX_ITERS = 10000;
     nStates = width(graphInfo.fwMatrix);
+
+        statsTable = table();
+        statsTable.active = nan(nStates, 1);
+        statsTable.visited = nan(nStates, 1);
+        statsTable.selected = nan(nStates, 1);
+        statsTable.next = nan(nStates, 1);
+        statsTable.new = nan(nStates, 1);
+        statsTable.pv = nan(nStates, 1);
+
+    pts = max(mainTable.p0, mainTable.p1)';
+
 
     wave = unique(initialStates);
 
@@ -20,8 +30,16 @@ function exploreWave_Faster(graphInfo, mainTable, initialStates)
         end
 
         nA = nnz(active);
-        waveSubset = find(active);
 
+           maxV = max(pts(active));
+           maxActive = active & pts == maxV;
+
+        if i < 0 % 12
+            waveSubset = find(active);
+        else    
+            waveSubset = ...find(active);
+                     find(maxActive);
+        end
 
         waveNext = moveWave(waveSubset, graphInfo.fwMatrix);
         waveNextU = unique([waveNext{:}]);
@@ -30,14 +48,31 @@ function exploreWave_Faster(graphInfo, mainTable, initialStates)
         waveNextD = waveNextU;
         waveNextD(visited(waveNextU)) = [];
 
+            foundFinals = any(mainTable.final(waveNextD));
+
         active(waveNextD) = true;
         active(waveSubset) = false;
         visited(waveSubset) = true;
         
+            
+
         fprintf('%d. A %d, sel %d, next %d, new %d\n', i, nA, numel(waveSubset), numel(waveNextU), numel(waveNextD))
     
-        % Now find 
-            initialStates = find(mainTable.final' & visited);
+            statsTable.active(i) = nA;
+            statsTable.visited(i) = nnz(visited);
+            statsTable.selected(i) = numel(waveSubset);
+            statsTable.next(i) = numel(waveNextU);
+            statsTable.new(i) = numel(waveNextD);
+            statsTable.pv(i) = maxV;
+
+        % Now find
+            if ~foundFinals
+                continue
+            end
+
+            initialStates = find(mainTable.final' & (visited));
+            %initialStates = find(mainTable.final' & (visited | active)); %
+            %     Somehow visited | active makes much slower search - even than unprunned  
             initialValues = mainTable.value(initialStates);
 
             newDiff = diffuse_New(graphInfo, mainTable, initialStates, initialValues);
@@ -52,18 +87,12 @@ function exploreWave_Faster(graphInfo, mainTable, initialStates)
                 unsolvedMat(:, ~isnan(newDiff)) = nan;
                 unsolvedSub = subgraphFrom(1, unsolvedMat);
 
-            % what if we clear 'active' where newDiff is not nan?
-               % active(~isnan(newDiff)) = false;
             % Not much changes in trial. We need stats: how many nodes
             % became solved on each level (step number)?
-               %active(solvedSub) = false; // this would cut off also
                activeUp = false(size(active));
                activeUp(unsolvedSub) = active(unsolvedSub);
-                     active = activeUp;
-               
-               % needed branches, if they share predecessor with an
-               % unneeded one
-
+                     
+                   active = activeUp;
     end
 
 
--- octave/new_dev.m
+++ octave/new_dev.m
@@ -1,95 +1,99 @@
 
 
-startValues = nan(1, nStates);
-startValues(finals) = 0;  %valueVector(finals);
-
-% Reversed graph info needed for this task
-revGraphInfo.fwMatrix = reverseMat;
-revGraphInfo.revMatrix = followerMat;
-
-revGraphInfo.fwW = getWeights_0(reverseMat, valueVector);
-revGraphInfo.revW = getWeights_0(followerMat, valueVector);
-
-revGraphInfo.eFrom = edgesTo; % ! reversed like the rest of this struct
-revGraphInfo.eTo = edgesFrom;
+statsTb = exploreWave_Faster(graphInfo, mainTable, 1);
 
 
-func = @(followerVals, weigths, ownVal) min(min(followerVals)+1, ownVal);
-
-%vs = generalDiffuse(graphInfo, startValues, find(finals), func);
-vs_New = generalDiffuse_New(revGraphInfo, startValues, find(finals), func);
-
-isequaln(vs_New, vs)
-
-%%
-
-    diffNew = diffuse_New(graphInfo, mainTable, find(finals), valueVector(finals));
-    
-    
-    exploreWave_Faster(graphInfo, mainTable, 1);
-
-
-%%
-clear graphInfo
-
-% Normal graph info this time 
-graphInfo.fwMatrix = followerMat;
-graphInfo.revMatrix = reverseMat;
-
-graphInfo.fwW = getWeights_0(followerMat, valueVector);
-graphInfo.revW = getWeights_0(reverseMat, valueVector);
-
-graphInfo.eFrom = edgesFrom;
-graphInfo.eTo = edgesTo;
-
-
-startSteps = inf(1, nStates);
-startSteps(1) = 1;
-
-steps_T = generalDiffuse(graphInfo, startSteps, 1, func);
-steps_TNew = generalDiffuse_New(graphInfo, startSteps, 1, func);
-
-isequaln(steps_T, stepValues)
-isequaln(steps_TNew, stepValues)
-
-%% rating - distance from optimal path (0 if on path)
-ratingFunc = @(fv, w, own) min(own, min(fv + 1 - w));
-rating = generalDiffuse_New(graphInfo, startSteps-1, 1, ratingFunc);
-
-%%
-
-%% A few example runs
-if false
-    explorationInput.valueVector = valueVector;
-    explorationInput.maxPoints = max(points0, points1);
-    explorationInput.moves = moves;
-    explorationInput.finals = finals;
-    explorationInput.tips = tips;
-    
-    
-  %  tic;  stats_a = exploreWave(followerMat, explorationInput, 100000, 'oldest'); time_a = toc;
-  %  tic;  stats_b = exploreWave(followerMat, explorationInput, 100000, 'newest');  time_b = toc;
-    tic;  stats_c = exploreWave(followerMat, explorationInput, inf, 'highV');  time_c = toc;
-    tic;  stats_d = exploreWave_Faster(followerMat, explorationInput, nan, '');  time_d = toc;
-    
-  %  time_a, time_b, 
-        time_c,time_d
-    % 
-    % figure; plotExploration(stats_a)
-    % figure; plotExploration(stats_b)
-    figure; plotExploration(stats_c)
-    figure; plotExploration(stats_d)
-end
-
-if false
-    optimals = markOptimalMoves(valueVector, followerMat);
- 
-    reached = findReachable(followerMat, optimals, moves);
-
-    initialStepsOpt = inf(1, numel(reached));
-    initialStepsOpt(reached) = 0;
-
-    % How many steps away from optimal path
-    stepsOpt = countStepsGeneral(followerMat, initialStepsOpt);
-    in4steps = stepsOpt <= 4;
-end
+% 
+% startValues = nan(1, nStates);
+% startValues(finals) = 0;  %valueVector(finals);
+% 
+% % Reversed graph info needed for this task
+% revGraphInfo.fwMatrix = reverseMat;
+% revGraphInfo.revMatrix = followerMat;
+% 
+% revGraphInfo.fwW = getWeights_0(reverseMat, valueVector);
+% revGraphInfo.revW = getWeights_0(followerMat, valueVector);
+% 
+% revGraphInfo.eFrom = edgesTo; % ! reversed like the rest of this struct
+% revGraphInfo.eTo = edgesFrom;
+% 
+% 
+% func = @(followerVals, weigths, ownVal) min(min(followerVals)+1, ownVal);
+% 
+% %vs = generalDiffuse(graphInfo, startValues, find(finals), func);
+% vs_New = generalDiffuse_New(revGraphInfo, startValues, find(finals), func);
+% 
+% isequaln(vs_New, vs)
+% 
+% %%
+% 
+%     diffNew = diffuse_New(graphInfo, mainTable, find(finals), valueVector(finals));
+% 
+% 
+%     exploreWave_Faster(graphInfo, mainTable, 1);
+% 
+% 
+% %%
+% clear graphInfo
+% 
+% % Normal graph info this time 
+% graphInfo.fwMatrix = followerMat;
+% graphInfo.revMatrix = reverseMat;
+% 
+% graphInfo.fwW = getWeights_0(followerMat, valueVector);
+% graphInfo.revW = getWeights_0(reverseMat, valueVector);
+% 
+% graphInfo.eFrom = edgesFrom;
+% graphInfo.eTo = edgesTo;
+% 
+% 
+% startSteps = inf(1, nStates);
+% startSteps(1) = 1;
+% 
+% steps_T = generalDiffuse(graphInfo, startSteps, 1, func);
+% steps_TNew = generalDiffuse_New(graphInfo, startSteps, 1, func);
+% 
+% isequaln(steps_T, stepValues)
+% isequaln(steps_TNew, stepValues)
+% 
+% %% rating - distance from optimal path (0 if on path)
+% ratingFunc = @(fv, w, own) min(own, min(fv + 1 - w));
+% rating = generalDiffuse_New(graphInfo, startSteps-1, 1, ratingFunc);
+% 
+% %%
+% 
+% %% A few example runs
+% if false
+%     explorationInput.valueVector = valueVector;
+%     explorationInput.maxPoints = max(points0, points1);
+%     explorationInput.moves = moves;
+%     explorationInput.finals = finals;
+%     explorationInput.tips = tips;
+% 
+% 
+%   %  tic;  stats_a = exploreWave(followerMat, explorationInput, 100000, 'oldest'); time_a = toc;
+%   %  tic;  stats_b = exploreWave(followerMat, explorationInput, 100000, 'newest');  time_b = toc;
+%     tic;  stats_c = exploreWave(followerMat, explorationInput, inf, 'highV');  time_c = toc;
+%     tic;  stats_d = exploreWave_Faster(followerMat, explorationInput, nan, '');  time_d = toc;
+% 
+%   %  time_a, time_b, 
+%         time_c,time_d
+%     % 
+%     % figure; plotExploration(stats_a)
+%     % figure; plotExploration(stats_b)
+%     figure; plotExploration(stats_c)
+%     figure; plotExploration(stats_d)
+% end
+% 
+% if false
+%     optimals = markOptimalMoves(valueVector, followerMat);
+% 
+%     reached = findReachable(followerMat, optimals, moves);
+% 
+%     initialStepsOpt = inf(1, numel(reached));
+%     initialStepsOpt(reached) = 0;
+% 
+%     % How many steps away from optimal path
+%     stepsOpt = countStepsGeneral(followerMat, initialStepsOpt);
+%     in4steps = stepsOpt <= 4;
+% end
